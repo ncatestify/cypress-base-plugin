2b57ceff675ba84c6206339f4726e8425c9068e8
Roland <rolandgolla@gmail.com>
2022-11-21 19:26:32 +0100

    Actual

--- cypress/e2e/validate.cy.js
+++ cypress/e2e/validate.cy.js
@@ -1,12 +1,11 @@
-"use strict";
 describe("Validate NCA Tests", () => {
   it("Command internal links is ok with status 200", () => {
     cy.everyInternalLinkStatusOk();
   });
-  it("Validate every page is loading", () => {
+  /* it("Validate every page is loading", () => {
     cy.everyInternalLinkIsLoading();
   });
   it("Validate imprint is clickable", () => {
     cy.validateImprintClickable();
-  });
+  }); */
 });
