b96eca269fbefe8732f01ee2bbf7567e1ae617c2
Roland <rolandgolla@gmail.com>
2022-11-21 19:16:03 +0100

    Actual

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://testify.team",
+    supportFile: "cypress/support/e2e.ts",
   },
   env: {
     waitForStartpage: "5000",
