6f1c386864f14630b911ebb4ce915f16b70f8b0f
Roland <rolandgolla@gmail.com>
2022-11-21 14:58:22 +0100

    Update config file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,3 +1,10 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({});
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "https://testify.team",
+  },
+  env: {
+    waitForStartpage: "5000",
+  },
+});
