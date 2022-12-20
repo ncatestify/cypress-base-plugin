7d6a3417903217a5515bd6b5709ab311d06b14a6
Roland <rolandgolla@gmail.com>
2022-12-20 23:15:05 +0100

    Add prefix to commands and use in validation link

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 /// <reference types="Cypress" />
 
-import { getInternalLinks } from "./commands/get-internal-links";
-import { validateImprintClickable } from "./commands/validate-imprint-clickable";
-import { everyInternalLinkStatusOk } from "./commands/every-internal-link-status-ok";
-import { everyInternalLinkIsLoading } from "./commands/every-internal-link-is-loading";
+import { getInternalLinks } from "./commands/nca-get-internal-links";
+import { validateImprintClickable } from "./commands/nca-validate-imprint-clickable";
+import { everyInternalLinkStatusOk } from "./commands/nca-every-internal-link-status-ok";
+import { everyInternalLinkIsLoading } from "./commands/nca-every-internal-link-is-loading";
 
 Cypress.Commands.add("getInternalLinks", getInternalLinks);
 Cypress.Commands.add("validateImprintClickable", validateImprintClickable);
