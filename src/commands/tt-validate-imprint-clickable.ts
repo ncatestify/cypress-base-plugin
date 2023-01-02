f430bcb6b2615860d329d2cadd1a149afbb34740
Roland <rolandgolla@gmail.com>
2023-01-02 23:29:45 +0100

    use a visible with contains to find imprint link and update cypress

--- src/commands/tt-validate-imprint-clickable.ts
+++ src/commands/tt-validate-imprint-clickable.ts
@@ -1,6 +1,5 @@
 export const ttValidateImprintClickable = () => {
   let oneLinkIsClickable = false;
   cy.log("validateImprintClickable - NCA TESTIFY");
-  cy.wait(5000);
-  cy.contains("a:visible", "impressum", { matchCase: false }).click();
+  cy.contains("a:visible", "mpressum", { matchCase: false }).click();
 };
