b135cef293f9a907be25899963d5858900586851
Roland <rolandgolla@gmail.com>
2023-01-04 12:03:33 +0100

    Update version to 2.0.6 an generate js files

---
+++ src/commands.js
@@ -0,0 +1,37 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import { ttEveryInternalLinkStatusOk } from "./commands/tt-every-internal-link-status-ok";
+import { ttEveryInternalLinkIsLoading } from "./commands/tt-every-internal-link-is-loading";
+import { ttGetInternalLinks } from "./commands/tt-get-internal-links";
+import { ttValidateImprintClickable } from "./commands/tt-validate-imprint-clickable";
+import { ttValidateNoGoogleFonts } from "./commands/tt-validate-no-google-fonts";
+import { ttElementExists } from "./commands/tt-element-exists";
+Cypress.Commands.add("ttEveryInternalLinkStatusOk", ttEveryInternalLinkStatusOk);
+Cypress.Commands.add("ttEveryInternalLinkIsLoading", ttEveryInternalLinkIsLoading);
+Cypress.Commands.add("ttGetInternalLinks", ttGetInternalLinks);
+Cypress.Commands.add("ttValidateImprintClickable", ttValidateImprintClickable);
+Cypress.Commands.add("ttValidateNoGoogleFonts", ttValidateNoGoogleFonts);
+Cypress.Commands.add("ttElementExists", ttElementExists);
---
+++ src/commands/tt-element-exists.js
@@ -0,0 +1,5 @@
+export const ttElementExists = (selector) => {
+    return cy
+        .window()
+        .then(($window) => $window.document.querySelector(selector));
+};
