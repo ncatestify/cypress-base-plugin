1d5d53d4109b0949f343e94991362b36f38a5df7
Lennard Tillmann <lennard@nevercodealone.de>
2023-01-06 14:41:54 +0100

    update example test to run ttRunTestifyBaseTests method

--- cypress/e2e/validate.cy.js
+++ cypress/e2e/validate.cy.js
@@ -3,16 +3,7 @@ describe("Validate Testify Tests", () => {
     beforeEach(() => {
         cy.visit("/");
     });
-    it("Validate imprint is clickable", () => {
-        cy.ttValidateImprintClickable();
-    });
-    it("Command internal links is ok with status 200", () => {
-        cy.ttEveryInternalLinkStatusOk();
-    });
-    it("Validate every page is loading", () => {
-        cy.ttEveryInternalLinkIsLoading();
-    });
-    it("Validates no google fonts are loading", () => {
-        cy.ttValidateNoGoogleFonts();
+    it('Runs Testify base tests', () => {
+        cy.ttRunTestifyBaseTests();
     });
 });
