d7be4a880f0cce4d900285c1ac7ee352406ad198
Roland <rolandgolla@gmail.com>
2023-04-03 00:00:45 +0200

    Fix promise error in google service command

--- src/commands/tt-validate-no-google-services.ts
+++ src/commands/tt-validate-no-google-services.ts
@@ -1,12 +1,14 @@
-export const ttValidateNoGoogleServices = () => {
-  cy.intercept({ method: 'GET', path: '*' }, (req) => {
+export const ttValidateNoGoogleServices = (): void => {
+  cy.on('request', (req) => {
     if (!req.url.startsWith(`${Cypress.config('baseUrl')}`)) {
-      cy.log('External url: ' + req.url);  
-      expect(req.url).not.to.include('/fonts.gstatic.com/')
-      expect(req.url).not.to.include('/fonts.googleapis.com/')
-      expect(req.url).not.to.include('/maps.google')
-      expect(req.url).not.to.include('/google.com/maps')
+      cy.log('External url: ' + req.url);
+      expect(req.url).not.to.include('/fonts.gstatic.com/');
+      expect(req.url).not.to.include('/fonts.googleapis.com/');
+      expect(req.url).not.to.include('/maps.google');
+      expect(req.url).not.to.include('/google.com/maps');
     }
-  })
-  cy.reload()
-}
+  });
+
+  cy.reload();
+};
+
