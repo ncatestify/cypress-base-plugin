d4e1c6e058b4dc84543ce09cc193d5815a31d782
Roland <rolandgolla@gmail.com>
2023-09-29 16:35:19 +0200

    Use alle akzeptieren in cookie command

--- src/commands/tt-cookie-all-accept.js
+++ src/commands/tt-cookie-all-accept.js
@@ -1,4 +1,4 @@
-export const ttCookieAllAcceptClick = (cookieButtonString = 'alle cookies') => {
+export const ttCookieAllAcceptClick = (cookieButtonString = 'alle akzeptieren') => {
     cy.log('ttCookieAllAcceptClick - NCA TESTIFY');
     return cy.contains(cookieButtonString, { matchCase: false }).click();
 };
