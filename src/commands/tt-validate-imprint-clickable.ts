bff10c5857ce9000b403d3c2d8fbe0cd31f3df79
Roland <rolandgolla@gmail.com>
2023-10-20 11:55:44 +0200

    Improve imprint link with better clickable method for modal links.

--- src/commands/tt-validate-imprint-clickable.ts
+++ src/commands/tt-validate-imprint-clickable.ts
@@ -1,20 +1,23 @@
-/**
- * Validates that the 'impressum' link is clickable and redirects to the correct page.
- * @returns A Cypress Chainable that resolves to a boolean indicating whether the link is clickable and redirects to the correct page.
- */
-export const ttValidateImprintClickable = (): Cypress.Chainable<boolean> => {
-  cy.log('validateImprintClickable - NCA TESTIFY')
+export const ttValidateImprintClickable = (): void => {
+  cy.get('a').each(($el, index, $list) => {
+    if ($el.text().toLowerCase().includes('impressum')) {
+      const element = $el.get(0);
+      const rect = element.getBoundingClientRect();
+      const elAtPoint = document.elementFromPoint(rect.left + rect.width / 2, rect.top + rect.height / 2);
+      const clickable = elAtPoint === element || element.contains(elAtPoint);
+      if (clickable) {
+        cy.wrap($el).click();
+        cy.window().then((win) => {
+          const isImpressum = win.location.href.includes('/impressum');
+          cy.expect(isImpressum).to.be.true;
+          return false; // to break out of the each loop
+        });
+      }
+    }
+  }).then(($list) => {
+    if ($list.length === 0) {
+      cy.expect(false).to.be.true; // Fails the test if no clickable link is found
+    }
+  });
+};
 
-  if (cy.ttElementExists('.sg-cookie-optin-box-footer-links')) {
-    cy.contains('.sg-cookie-optin-box-footer-links a', 'impressum', {
-      matchCase: false
-    }).click()
-  } else {
-    cy.wait(5000)
-    cy.contains('a:visible', 'impressum', { matchCase: false }).click()
-  }
-
-  return cy.window().then((win) => {
-    return win.location.href.includes('/impressum')
-  })
-}
