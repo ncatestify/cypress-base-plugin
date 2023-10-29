3f92ed53eb5b1c87670791d961fd658ff565b37f
Roland <rolandgolla@gmail.com>
2023-10-29 20:51:33 +0100

    Only use typescript tests

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,8 @@ import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts?(x)']
 }
 
 export default config
