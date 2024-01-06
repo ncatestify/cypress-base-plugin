8fa68dccdaf42aa46e47991a5b6cc8faa3b47266
Roland <rolandgolla@gmail.com>
2024-01-06 12:08:02 +0100

    Move the jest tests to root folder.

---
+++ dist/__test__/isInternal.test.js
@@ -0,0 +1,16 @@
+import { isInternal } from '../utils/isInternal';
+import { expect } from '@jest/globals';
+describe('isInternal', () => {
+    test('returns true for internal URLs', () => {
+        expect(isInternal('/path/to/page')).toBe(true);
+    });
+    test('returns false for external URLs', () => {
+        expect(isInternal('https://www.example.com')).toBe(false);
+    });
+    test('returns true for wildcard protocol matching baseUrl', () => {
+        expect(isInternal('//localhost:3000/path/to/page')).toBe(true);
+    });
+    test('returns false for wildcard protocol not matching baseUrl', () => {
+        expect(isInternal('//www.instagram.com/profile')).toBe(false);
+    });
+});
