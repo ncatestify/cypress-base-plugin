ef206a4a73048b7c6645c46f6b4e2ba74ff25389
Roland <rolandgolla@gmail.com>
2024-01-14 23:44:26 +0100

    Improve methods and internal link tests.

--- __test__/isInternal.test.ts
+++ __test__/isInternal.test.ts
@@ -2,19 +2,31 @@ import { isInternal } from '../src/utils/isInternal'
 import { expect } from '@jest/globals'
 
 describe('isInternal', () => {
+  beforeEach(() => {
+    process.env.BASE_URL = 'http://localhost:3000'
+  })
+
   test('returns true for internal URLs', () => {
     expect(isInternal('/path/to/page')).toBe(true)
   })
 
-  test('returns false for external URLs', () => {
-    expect(isInternal('https://www.example.com')).toBe(false)
+  test('returns true for wildcard protocol matching baseUrl', () => {
+    expect(isInternal('http://localhost:3000/path/to/page')).toBe(true)
   })
 
   test('returns true for wildcard protocol matching baseUrl', () => {
     expect(isInternal('//localhost:3000/path/to/page')).toBe(true)
   })
 
+  test('returns false for external URLs', () => {
+    expect(isInternal('https://www.example.com')).toBe(false)
+  })
+
   test('returns false for wildcard protocol not matching baseUrl', () => {
     expect(isInternal('//www.instagram.com/profile')).toBe(false)
+  })
+
+  afterEach(() => {
+    delete process.env.BASE_URL
   })
 })
--- src/commands/tt-get-internal-links.ts
+++ src/commands/tt-get-internal-links.ts
@@ -3,33 +3,34 @@ import { isInternal } from '../utils/isInternal'
 export const ttGetInternalLinks = (): Cypress.Chainable<string[]> => {
   cy.log('ttGetInternalLinks - NCA TESTIFY')
   const listOfResults: string[] = []
-  cy.get('a').each((resultItem) => {
-    let singleResult = ''
-    cy.wrap(resultItem)
-      .invoke('attr', 'href')
-      .then((href: string | undefined) => {
-        if (
-          href != null &&
-          isInternal(href) &&
-          !href.includes('mailto') &&
-          !href.includes('tel') &&
-          !listOfResults.includes(href)
-        ) {
-          const baseUrl = Cypress.config('baseUrl') as string
-          singleResult = href.replace(baseUrl, '')
-        } else if (href != null) {
-          cy.log('Filtered URL: ' + href)
-        } else {
-          cy.log('Empty or null URL')
-        }
-      })
 
-    cy.then(() => {
-      if (singleResult.length !== 0) {
-        listOfResults.push(singleResult)
+  return cy.get('a').then((anchorElements) => {
+    const processLink = (index) => {
+      if (index >= anchorElements.length) {
+        return listOfResults // Alle Links wurden verarbeitet
       }
-    })
-  })
 
-  return cy.wrap(listOfResults, { log: false })
+      const href = anchorElements[index].getAttribute('href')
+      if (
+        href &&
+        isInternal(href) &&
+        !href.includes('mailto') &&
+        !href.includes('tel')
+      ) {
+        const baseUrl = Cypress.config('baseUrl')
+        const singleResult = href.replace(baseUrl, '')
+        if (!listOfResults.includes(singleResult)) {
+          listOfResults.push(singleResult)
+        }
+      } else if (href) {
+        cy.log('Filtered URL: ' + href)
+      } else {
+        cy.log('Empty or null URL')
+      }
+
+      return cy.then(() => processLink(index + 1))
+    }
+
+    return processLink(0)
+  })
 }
