9414b3581d89aea7dd92aa427b1d2d959d4a34a7
Lennard Tillmann <lennard@nevercodealone.de>
2024-03-08 15:37:02 +0100

    Update command and version

--- dist/commands/tt-validate-language-tag.js
+++ dist/commands/tt-validate-language-tag.js
@@ -1,19 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ttValidateLanguageTag = void 0;
-const ttValidateLanguageTag = (language = '') => {
+const ttValidateLanguageTag = (language = 'de') => {
     cy.log('ttValidateLanguageTag - NCA TESTIFY');
     cy.get('html').should('have.attr', 'lang');
     cy.get('html')
         .invoke('attr', 'lang')
         .then((langTag) => {
         cy.wrap(langTag).then((lang) => {
-            if (language !== '') {
-                expect(lang.toLowerCase()).to.contain(language);
-            }
-            else {
-                expect(lang.toLowerCase()).to.contain('de');
-            }
+            expect(lang.toLowerCase()).to.contain(language);
         });
     });
 };
--- src/commands/tt-validate-language-tag.ts
+++ src/commands/tt-validate-language-tag.ts
@@ -1,15 +1,11 @@
-export const ttValidateLanguageTag = (language: string = ''): void => {
+export const ttValidateLanguageTag = (language: string = 'de'): void => {
   cy.log('ttValidateLanguageTag - NCA TESTIFY')
   cy.get('html').should('have.attr', 'lang')
   cy.get('html')
     .invoke('attr', 'lang')
     .then((langTag) => {
       cy.wrap(langTag).then((lang) => {
-        if (language !== '') {
-          expect(lang.toLowerCase()).to.contain(language)
-        } else {
-          expect(lang.toLowerCase()).to.contain('de')
-        }
+        expect(lang.toLowerCase()).to.contain(language)
       })
     })
 }
