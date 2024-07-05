69ee0a1fc09d691e828d6c28ee60c40f9e5d61fb
Roland <rolandgolla@gmail.com>
2024-07-05 13:49:02 +0200

    Add new test to validate.cy.ts.

--- cypress/e2e/validate.cy.ts
+++ cypress/e2e/validate.cy.ts
@@ -54,6 +54,12 @@ describe('Validate Testify Tests', () => {
   })
 
   it('Set up console error listener', () => {
+    cy.visit('/console-errors/')
+    cy.ttSetupConsoleErrorListener()
+  })
+
+  it('Check if multiple css files are missing', () => {
+    cy.visit('/css-file-not-loading/')
     cy.ttSetupConsoleErrorListener()
   })
 
