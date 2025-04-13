496229e101938cc4a8b9cb9e23a2304e7bc8209a
Roland <rolandgolla@gmail.com>
2025-04-14 00:16:43 +0200

    Improve type definitions for custom commands for better TypeScript compatibility

--- cypress/index.d.ts
+++ cypress/index.d.ts
@@ -1,20 +1,24 @@
 declare namespace Cypress {
   interface Chainable<Subject = any> {
     ttAccessibility(context?: any, options?: any): Chainable<Subject>
-    ttCookieAllAcceptClick(): void
-    ttDetectHttp(): void
-    ttElementExists(element: string): boolean
-    ttEveryInternalLinkIsLoading(): void
-    ttEveryInternalLinkStatusOk(): void
-    ttGetInternalLinks<Subject>(): Chainable<string[]>
-    ttInvalidPath404(): void
-    ttOnlyOneH1(): void
-    ttRunTestifyBaseTests(): void
-    ttValidateAllImagesResponseStatusOk(): void
-    ttValidateImprintClickable(): void
-    ttValidateLanguageTag(language: string): void
-    ttValidateNoGoogleServices(): void
-    ttValidateSubpagesAndImages(limit?: number, linkSelector?: string): void
-    ttValidatePageContent(): void
+    ttClickIfElementExist(element: string): Chainable<Subject>
+    ttCookieAllAcceptClick(): Chainable<Subject>
+    ttDetectHttp(): Chainable<Subject>
+    ttElementExists(element: string): Chainable<boolean>
+    ttEveryInternalLinkIsLoading(): Chainable<Subject>
+    ttEveryInternalLinkStatusOk(): Chainable<Subject>
+    ttGetInternalLinks(linkSelector?: string): Chainable<string[]>
+    ttInvalidPath404(): Chainable<Subject>
+    ttOnlyOneH1(): Chainable<Subject>
+    ttPageLoaded(): Chainable<Subject>
+    ttRunTestifyBaseTests(): Chainable<Subject>
+    ttSetupConsoleErrorListener(): Chainable<Subject>
+    ttThreshold(threshold?: number): Chainable<Subject>
+    ttValidateAllImagesResponseStatusOk(): Chainable<Subject>
+    ttValidateImprintClickable(): Chainable<Subject>
+    ttValidateLanguageTag(language: string): Chainable<Subject>
+    ttValidateNoGoogleServices(): Chainable<Subject>
+    ttValidatePageContent(): Chainable<Subject>
+    ttValidateSubpagesAndImages(limit?: number, linkSelector?: string): Chainable<Subject>
   }
 }
