878239970e09a6d7740d4407deb808aa67f447eb
Roland <rolandgolla@gmail.com>
2025-05-18 22:30:19 +0200

    Fix TypeScript issue with cypress-axe types
    - Added @ts-ignore comments for cypress-axe methods
    - Resolves build errors with injectAxe and checkA11y

--- dist/src/commands/tt-accessibility.js
+++ dist/src/commands/tt-accessibility.js
@@ -3,7 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ttAccessibility = void 0;
 const ttAccessibility = () => {
     cy.log('ttAccessibility - NCA TESTIFY');
+    // @ts-ignore - cypress-axe types
     cy.injectAxe();
+    // @ts-ignore - cypress-axe types
     cy.checkA11y();
 };
 exports.ttAccessibility = ttAccessibility;
--- src/commands/tt-accessibility.ts
+++ src/commands/tt-accessibility.ts
@@ -1,6 +1,8 @@
 export const ttAccessibility = (): void => {
   cy.log('ttAccessibility - NCA TESTIFY')
 
+  // @ts-ignore - cypress-axe types
   cy.injectAxe()
+  // @ts-ignore - cypress-axe types
   cy.checkA11y()
 }
