2d0ae2013c114cac8ffb7fe3e11d3eeb1e9468b9
Roland <rolandgolla@gmail.com>
2025-06-18 00:01:30 +0200

    Fix dist file compilation error by removing stale tt-run-testify-base-tests import
    The dist directory contained an outdated reference to the removed tt-run-testify-base-tests command, causing compilation errors. Rebuilt the project to sync source and dist files properly.

--- dist/api-version.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.APIVersion = void 0;
-var APIVersion;
-(function (APIVersion) {
-    APIVersion["latest"] = "latest";
-    APIVersion["default"] = "default";
-})(APIVersion = exports.APIVersion || (exports.APIVersion = {}));
