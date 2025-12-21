ae14b90f235823e9626d68bfb7e57d36d8c0853c
Roland Golla <rolandgolla@gmail.com>
2025-12-21 22:39:58 +0100

    Update tt-only-one-h1 to use cy.ttEl

--- dist/commands/tt-only-one-h1.js
+++ dist/commands/tt-only-one-h1.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ttOnlyOneH1 = void 0;
 const ttOnlyOneH1 = () => {
     cy.log('ttOnlyOneH1 - NCA TESTIFY');
-    cy.get('h1').its('length').should('eq', 1);
+    // @ts-ignore - Custom command registered at runtime
+    cy.ttEl('h1', 'h1Heading').its('length').should('eq', 1);
 };
 exports.ttOnlyOneH1 = ttOnlyOneH1;
